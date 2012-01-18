7997a18a63482566cf80b5d812ad5157853da154
Axel Bengtsson <axel.bengtsson@gmail.com>
2012-01-18 23:10:18 +0100

    Added objects in objects
    Now the user can set a.b and
    the json string will be:
     { a : {b : X}}
    
    Also added --newline that make the
    program operate on multi lined strings.

---
+++ test/ab/json/shell/test/TestToJsonMain.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package ab.json.shell.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import ab.json.shell.ToJsonMain;
+
+/**
+ * @author Axel Bengtsson
+ *
+ */
+public class TestToJsonMain {
+
+	@Test
+	public void testCommonSpaceSplit() {
+		final String[] args = {"--file=test-files/test-common.txt", "a.b", "a.c", "a.d.e", "b"};
+		assertTrue(new ToJsonMain().init(args) == 0);
+	}
+
+	@Test
+	public void testRegexSplit() {
+		final String[] args = { "--file=test-files/test-regex.txt",
+		    "--regex=.*par1=\"(.+)\" par2=\"(.+)\" par3=\"(.+)\".*", "a.b", "a.c",
+		    "a.d" };
+		assertTrue(new ToJsonMain().init(args) == 0);
+	}
+
+	@Test
+	public void testPackages() {
+		final String[] args = {"--file=test-files/test-packages.txt", "--newline=",
+				"--regex=.*Package\\: (.+) Version\\: (.+?) .*", "name", "version"};
+		assertTrue(new ToJsonMain().init(args) == 0);
+	}
+}
